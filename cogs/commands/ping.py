3b155fcd6dfceb5d6d71a882f4588249a816b9c1
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-14 14:15:01 +0200

    Linting /joke and /ping

--- cogs/commands/ping.py
+++ cogs/commands/ping.py
@@ -1,14 +1,28 @@
+"""
+Command: /ping
+Description: Pongs back, mostly just to test connectivity
+"""
 from revolt.ext import commands
-from client import Zorak
+
+from client import Zorak  # pylint: disable=E0401
 
 
 class Ping(commands.Cog[Zorak]):
+    """
+    Hosts only the 1 command.
+    """
 
     @commands.command()
     async def ping(self, ctx: commands.Context):
+        """
+        This message satisfies the linter.
+        It's a ping, what more do you want?
+        """
         await ctx.send("pong")
 
 
 def setup(client: Zorak):
+    """
+    Loads the cog into the Client (Zorak)
+    """
     client.add_cog(Ping())
-
