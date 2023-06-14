fe76005deef69a4d7ba10c6949200185b5f4bc39
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-14 14:16:47 +0200

    Linting /quote

--- cogs/commands/quote.py
+++ cogs/commands/quote.py
@@ -1,17 +1,34 @@
-from revolt.ext import commands
-from client import Zorak
-
+"""
+Command: /quote
+Description: Sends an inspiring quote
+"""
 import json
 import requests
 
+from revolt.ext import commands
+from client import Zorak  # pylint: disable=E0401
+
 
 class Quote(commands.Cog[Zorak]):
+    """
+    Hosts only the 1 command.
+    """
 
     @commands.command()
     async def quote(self, ctx):
-        quote = json.loads(requests.get("https://zenquotes.io/api/random").text)[0]
+        """
+        Sends a quote from zenquotes
+        """
+        quote = json.loads(
+            requests.get(
+                "https://zenquotes.io/api/random"
+                , timeout=5
+            ).text)[0]
         await ctx.send((quote["q"] + "\n- " + quote["a"]))
 
 
 def setup(client: Zorak):
+    """
+    Loads the cog into the Client (Zorak)
+    """
     client.add_cog(Quote())
