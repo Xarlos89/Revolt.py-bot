ff43b6cf2a304f7706403ae53f8e87c6757f5b8c
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-14 14:20:19 +0200

    Linting /taunt

--- cogs/commands/taunt.py
+++ cogs/commands/taunt.py
@@ -1,17 +1,28 @@
-from revolt.ext import commands
-from client import Zorak
-
-from bs4 import BeautifulSoup
+"""
+Command: /taunt
+Description: Sends a classical shakespearian taunt
+"""
 import requests
+from bs4 import BeautifulSoup
+from revolt.ext import commands
+
+from client import Zorak  # pylint: disable=E0401
 
 
 class Taunt(commands.Cog[Zorak]):
+    """
+    Hosts only the 1 command.
+    """
 
     @commands.command()
     async def taunt(self, ctx):
+        """
+        Sends a taunt that we scrape using bs4
+        """
         taunt = BeautifulSoup(
             requests.get(
                 "https://fungenerators.com/random/insult/shakespeare/"
+                , timeout=5
             ).content,
             "html.parser",
         ).find("h2")
@@ -19,8 +30,7 @@ class Taunt(commands.Cog[Zorak]):
 
 
 def setup(client: Zorak):
+    """
+    Loads the cog into the Client (Zorak)
+    """
     client.add_cog(Taunt())
-
-
-
-
