5ad8e96f4f8a56aa6ef6787ede8e17ad22f5c4f2
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-14 14:11:23 +0200

    Linting /google

--- cogs/commands/google.py
+++ cogs/commands/google.py
@@ -1,15 +1,30 @@
+"""
+Command: /google
+Description: Sarcastically googles something
+"""
 from revolt.ext import commands
-from client import Zorak
+
+from client import Zorak  # pylint: disable=E0401
 
 
 class Google(commands.Cog[Zorak]):
+    """
+    Hosts only the 1 command.
+    """
 
     @commands.command()
     async def google(self, ctx, *question):
+        """
+        Just sends a sarcastic link.
+        """
         await ctx.send(
-            f"Here, allow me to google that one for you:\nhttps://letmegooglethat.com/?q={'+'.join(question)}"
+            f"Here, allow me to google that one for you: \
+            \nhttps://letmegooglethat.com/?q={'+'.join(question)}"
         )
 
 
 def setup(client: Zorak):
+    """
+    Loads the cog into the Client (Zorak)
+    """
     client.add_cog(Google())
